8bace79017161e4e2dadc032499c73c6374151fa
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-26 06:09:44 -0700

    fileboj method must be openend in binary mode

--- download-file.py
+++ download-file.py
@@ -2,3 +2,12 @@ import boto3
 
 s3 = boto3.client('s3')
 s3.download_file('BUCKET_NAME', 'OBJECT_NAME', 'FILE_NAME')
+
+"""
+The download_fileobj method accepts a writeable file-like object. 
+The file object must be opened in binary mode, not text mode.
+
+s3 = boto3.client('s3')
+with open('FILE_NAME', 'wb') as f:
+    s3.download_fileobj('BUCKET_NAME', 'OBJECT_NAME', f)
+"""
